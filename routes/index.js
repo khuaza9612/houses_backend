ecae75960acd7fe3512ef07dd2c5c2ab462eceb1
khuaza9612 <andresnavia-96@hotmail.com>
2022-10-18 11:03:58 -0500

    .

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+const router = Router();
+//const verifyToken = require('../middlewares/authenticate');
+
+
+ 
+// router.use(require('./Reviews.js'));
+
+module.exports = router;
